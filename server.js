4f4b107bcde99a3ec693e4da993605ae694630cf
Enes İzgi <enesizgi@tutanota.com>
2022-02-27 03:01:03 +0300

    server.js :: Remove old comments

--- server.js
+++ server.js
@@ -19,32 +19,6 @@ app.get('/departments', async (req, res) => {
 });
 
 app.get('/courses', async (req, res) => {
-    // console.log(req);
-    // const departmentsURL = 'https://oibs2.metu.edu.tr/View_Program_Course_Details_64/main.php';
-    // const departments = await sendRequest(departmentsURL);
-    // const dom = new JSDOM(departments, { url: departmentsURL });
-    // const { document } = dom.window;
-    // const options = Array.from(document.querySelectorAll('option'));
-    // const filteredOptions = options.filter(option => {
-    //     if (option.text.includes('Spring') || option.text.includes('Fall') || option.text.includes('Summer School')) {
-    //         return false;
-    //     }
-    //     return true;
-    // });
-    // const simplifiedOptions = filteredOptions.map(option => {
-    //     return {
-    //         value: option.value,
-    //         text: option.text
-    //     }
-    // });
-    // const optionsJSON = {
-    //     result: simplifiedOptions
-    // }
-    // res.json(optionsJSON);
-    // res.json({
-    //     result: []
-    // });
-
     fs.readFile('courses.json', 'utf8', (err, data) => {
         if (err) {
             console.log(err);
